3c1ea2bf9263be55e68f949f6c86bb81e1e7fd71
Daya <d.udgiri@gmail.com>
2021-11-12 16:05:46 +0530

    modified app.py, index.html and results.html

--- app.py
+++ app.py
@@ -3,8 +3,7 @@
 from flask import Flask, render_template, request,jsonify
 from flask_cors import CORS,cross_origin
 import pickle
-import pandas as pd
-from patsy import dmatrix
+from train import scaler
 
 app = Flask(__name__) # initializing a flask app
 
@@ -19,39 +18,32 @@ def index():
     if request.method == 'POST':
         try:
             #  reading the inputs given by the user
-            rate_marriage = float(request.form['rate_marriage'])
-            age = float(request.form['age'])
-            yrs_married = float(request.form['yrs_married'])
-            children = float(request.form['children'])
-            religious = float(request.form['religious'])
-            educ = float(request.form['educ'])
-            occupation = float(request.form['occupation'])
-            occupation_husb = float(request.form['occupation_husb'])
+            Pclass = float(request.form['Pclass'])
+            Sex = str(request.form['Sex'])
+            Age = float(request.form['Age'])
+            SibSp = float(request.form['SibSp'])
+            Parch = float(request.form['Parch'])
+            Fare = float(request.form['Fare'])
 
-            input_df = pd.DataFrame(data=[[rate_marriage, age, yrs_married,children,religious,educ,occupation,occupation_husb]],
-                                    columns=["rate_marriage","age", "yrs_married","children","religious","educ","occupation","occupation_husb"])
+            if Sex=="male":
+                male=1
+                female=0
+            else:
+                male=0
+                female=1
 
-            X_input = dmatrix('rate_marriage + age + yrs_married + children + \
-            religious + educ + occupation + occupation_husb', input_df, return_type="dataframe")
+            # input_df = pd.DataFrame(data=[[Pclass, Sex, Age, SibSp,Parch,Fare]], \
+            #                         columns=["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare"])
 
-            print(X_input)
+            # X_input = dmatrix('rate_marriage + age + yrs_married + children + \
+            # religious + educ + occupation + occupation_husb', input_df, return_type="dataframe")
 
-            # X_input = X_input.rename(columns=
-            #              {'C(occupation)[T.2.0]': 'occ_2',
-            #               'C(occupation)[T.3.0]': 'occ_3',
-            #               'C(occupation)[T.4.0]': 'occ_4',
-            #               'C(occupation)[T.5.0]': 'occ_5',
-            #               'C(occupation)[T.6.0]': 'occ_6',
-            #               'C(occupation_husb)[T.2.0]': 'occ_husb_2',
-            #               'C(occupation_husb)[T.3.0]': 'occ_husb_3',
-            #               'C(occupation_husb)[T.4.0]': 'occ_husb_4',
-            #               'C(occupation_husb)[T.5.0]': 'occ_husb_5',
-            #               'C(occupation_husb)[T.6.0]': 'occ_husb_6'})
+            # print(X_input)
 
-            filename = 'Logi_saga_final_model.pickle'
+            filename = 'Decision_Tree_opti_model.pickle'
             loaded_model = pickle.load(open(filename, 'rb')) # loading the model file from the storage
             # predictions using the loaded model file
-            prediction = loaded_model.predict(X_input)
+            prediction = loaded_model.predict(scaler.transform([[Pclass, Age, SibSp,Parch,Fare, female, male]]))
             print('prediction is', prediction)
             # showing the prediction results in a UI
             return render_template('results.html',prediction=round(prediction[0],2))
